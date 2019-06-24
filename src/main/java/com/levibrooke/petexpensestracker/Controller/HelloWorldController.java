c2289fdadb1b25f1710ebb4e168fe22a361d492b
Levi Porter <levi@levibrooke.com>
2019-06-24 12:00:27 -0700

    Move controller into controller dir

--- src/main/java/com/levibrooke/petexpensestracker/Controller/HelloWorldController.java
+++ src/main/java/com/levibrooke/petexpensestracker/Controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.levibrooke.petexpensestracker;
+package com.levibrooke.petexpensestracker.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
