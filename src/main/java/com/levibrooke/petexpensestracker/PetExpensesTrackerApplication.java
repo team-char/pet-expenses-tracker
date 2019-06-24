7d80be83e577187ed83a8b9e04f9f7b0ceac1d7d
Levi Porter <levi@levibrooke.com>
2019-06-24 11:41:01 -0700

    First commit

---
+++ src/main/java/com/levibrooke/petexpensestracker/PetExpensesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.levibrooke.petexpensestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetExpensesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetExpensesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levibrooke/petexpensestracker/PetExpensesTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.levibrooke.petexpensestracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetExpensesTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
