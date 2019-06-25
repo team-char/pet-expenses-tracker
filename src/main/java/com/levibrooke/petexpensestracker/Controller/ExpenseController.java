d3f78abb99c5fe077cd1d5809e771760b4c98d04
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-06-25 14:04:04 -0700

    refactored form, model and controller expenses

--- src/main/java/com/levibrooke/petexpensestracker/Controller/ExpenseController.java
+++ src/main/java/com/levibrooke/petexpensestracker/Controller/ExpenseController.java
@@ -1,5 +1,4 @@
 package com.levibrooke.petexpensestracker.Controller;
-
 import com.levibrooke.petexpensestracker.Model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,15 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
-
-import javax.persistence.Table;
 import java.security.Principal;
 import java.text.ParseException;
-
 import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.List;
-
 
 @Controller
 public class ExpenseController {
@@ -33,29 +27,42 @@ public class ExpenseController {
     CategoryRepository categoryRepository;
 
     // GET: route to expense form
-    @GetMapping("/createExpense")
-    public String getFormExpense(Model m){
+    @GetMapping("/create-expense")
+    public String getFormExpense(Principal p, Model m){
         //get all categories to add to form this works when we MANUALLY add categories to DB
-        List<Category> allCategories = (List<Category>) categoryRepository.findAll();
-        m.addAttribute("categoryList", allCategories);
-        return "/createExpense";
+//        List<Category> allCategories = (List<Category>) categoryRepository.findAll();
+//        m.addAttribute("categoryList", allCategories);
+        HomeController.isUserLoggedIn(p, m);
+        return "/create-expense";
     }
 
     //POST: route to post  one expense
-    @PostMapping("/createExpense")
-    public RedirectView postExpense(@RequestParam String name, @RequestParam Date createdAt,
-                                    @RequestParam String description,
-                                    @RequestParam Double amount, @RequestParam Long categoryId, Principal p) throws ParseException {
+    @PostMapping("/create-expense")
+    public RedirectView postExpense(@RequestParam Date createdAt, @RequestParam String description,
+                                    @RequestParam Double amount, @RequestParam String categoryName, Principal p,
+                                    Model m) throws ParseException {
         AppUser currentUser = (AppUser) ((UsernamePasswordAuthenticationToken) p).getPrincipal();
 
         //still need category
-
-
-        Expense newExpense = new Expense(name, description, amount, createdAt, categoryId);
+        Expense newExpense = new Expense(description, amount, createdAt, categoryName);
         newExpense.appUser = appUserRepository.findById(currentUser.getId()).get();
         expenseRepository.save(newExpense);
 
+        HomeController.isUserLoggedIn(p, m);
+
         return new RedirectView("/dashboard");
     }
+
+    @GetMapping("/all-expense")
+    public String getAllExpensePage(Principal p, Model m){
+        List<Expense> expenseList = (List<Expense>) expenseRepository.findAll();
+        m.addAttribute(expenseList);
+        HomeController.isUserLoggedIn(p, m);
+        return "/all-expense";
+    }
+
+
+
+
 
 }
--- src/main/java/com/levibrooke/petexpensestracker/Model/Expense.java
+++ src/main/java/com/levibrooke/petexpensestracker/Model/Expense.java
@@ -4,9 +4,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.sql.Date;
-import java.util.List;
 
 @Entity
 @Table(name="expense")
@@ -18,9 +16,8 @@ public class Expense {
 
     @DateTimeFormat(pattern = "MM-dd-yyyy")
     Date createdAt;
-    String name;
     String description;
-    Long categoryId;
+    String categoryName;
 
     @NumberFormat(style = NumberFormat.Style.CURRENCY)
     Double amount;
@@ -28,24 +25,18 @@ public class Expense {
     @ManyToOne
     public AppUser appUser;
 
-//    @ManyToOne
-//    public Long category;
-
     public Expense(){}
 
-    public Expense(String name, String description, Double amount, Date createdAt, Long categoryId) {
-        this.name = name;
+    public Expense(String description, Double amount, Date createdAt, String categoryName) {
         this.description = description;
         this.amount = amount;
         this.createdAt = createdAt;
-        this.categoryId = categoryId;
+        this.categoryName = categoryName;
     }
 
     public long getId() { return id; }
 
     public Date getCreatedAt() { return createdAt; }
-
-    public String getName() { return name; }
 
     public String getDescription() { return description; }
 
@@ -53,21 +44,14 @@ public class Expense {
 
     public AppUser getAppUser() { return appUser; }
 
-//    public Long getCategory() { return category; }
-
     public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
-
-    public void setName(String name) { this.name = name; }
 
     public void setDescription(String description) { this.description = description; }
 
     public void setAmount(Double amount) { this.amount = amount; }
 
-    public Long getCategoryId() { return categoryId; }
+    public String getCategoryName() { return categoryName; }
 
-    public void setCategoryId(Long categoryId) { this.categoryId = categoryId; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
 
-//    public void setCategory(Long category) { this.category = category; }
-
-//    public void setAppUser(AppUser appUser) { this.appUser = appUser; }
 }
